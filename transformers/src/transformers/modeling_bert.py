b988e958d1d355821938ea20894b60981ec3503c
yao12315 <76727843+yao12315@users.noreply.github.com>
2023-01-11 13:39:44 +0800

    Update modeling_bert.py

--- transformers/src/transformers/modeling_bert.py
+++ transformers/src/transformers/modeling_bert.py
@@ -743,7 +743,7 @@ class BertModel(BertPreTrainedModel):
         # positions we want to attend and -10000.0 for masked positions.
         # Since we are adding it to the raw scores before the softmax, this is
         # effectively the same as removing these entirely.
-        extended_attention_mask = extended_attention_mask.to(dtype=next(self.parameters()).dtype)  # fp16 compatibility
+        extended_attention_mask = extended_attention_mask.to(dtype=torch.float32)  # fp16 compatibility
         extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
 
         # If a 2D ou 3D attention mask is provided for the cross-attention
